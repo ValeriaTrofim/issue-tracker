91968b434b638f4284601d777ceca302ac11db4c
Valeria Trofim <valeria.trofim17@gmail.com>
2024-07-27 14:16:22 +0200

    Set up Radix UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
-import Image from 'next/image'
+import React from 'react'
+import { Button } from '@radix-ui/themes';
 
-export default function Home() {
+const IssuesPage = () => {
   return (
-    <div>Hello World</div>)
+    
+    <div><Button>New Issue</Button></div>
+  )
 }
+
+export default IssuesPage
