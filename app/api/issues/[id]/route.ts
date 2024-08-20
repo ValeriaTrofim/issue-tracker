f9b615c317c487bf04f7e02c02214bc4e8381544
Valeria Trofim <valeria.trofim17@gmail.com>
2024-08-20 03:13:03 +0200

    Secure the application

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,11 +1,19 @@
+import authOptions from "@/app/auth/authOptions";
 import { IssueSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function PATCH(
   request: NextRequest, 
   { params }: { params: { id: string }}) {
-  const body = await request.json();
+
+  const session = await getServerSession(authOptions)
+  if(!session) 
+    return NextResponse.json({}, {status: 401})  
+
+  const body = await request.json()
+
   const validation = IssueSchema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 });
@@ -31,6 +39,10 @@ export async function PATCH(
   export async function DELETE(
     request: NextRequest, 
     { params }: { params: { id: string }}) {
+
+    const session =await getServerSession(authOptions)
+    if(!session)
+       return NextResponse.json({}, {status: 401})  
   
     const issue = await prisma.issue.findUnique({
       where: { id: parseInt(params.id) }
