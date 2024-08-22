87208976eaa34b9de56938702e57533780f94140
Valeria Trofim <valeria.trofim17@gmail.com>
2024-08-22 22:10:04 +0200

    Fetch data for asignee with React Query

--- app/issues/[id]/AsigneeSelect.tsx
+++ app/issues/[id]/AsigneeSelect.tsx
@@ -1,17 +1,32 @@
 
 'use client'; 
 
-import { Select } from '@radix-ui/themes'
+import { User } from '@prisma/client';
+import { Select, Skeleton } from '@radix-ui/themes'
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 import React from 'react'
 
 const AssigneeSelect = () => {
+    const { data: users, error, isLoading } = useQuery<User[]>({
+        queryKey: ['users'],
+        queryFn: () => axios.get('/api/users').then(res => res.data),
+        staleTime: 60 * 1000, //60s
+        retry: 3
+      });
+
+    if (isLoading) return <Skeleton />;  
+    if (error) return null;
+      
   return (
     <Select.Root>
       <Select.Trigger placeholder='Assign...' />
       <Select.Content>
          <Select.Group>
             <Select.Label>Suggestions</Select.Label>
-            <Select.Item value="1">Valeria Trofim</Select.Item>
+            {users?.map(user => (
+              <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>)
+            )}
          </Select.Group>
       </Select.Content>
     </Select.Root>
