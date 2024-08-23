7487f7db0d8890e77970a6e3e87a966535a1141f
Valeria Trofim <valeria.trofim17@gmail.com>
2024-08-23 16:18:56 +0200

    Refactor AsigneeSelect component

--- app/issues/[id]/AsigneeSelect.tsx
+++ app/issues/[id]/AsigneeSelect.tsx
@@ -11,35 +11,25 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   const [assigneeField, setAssigneeField] = useState(
     issue.assignedToUserId || "unassigned"
   );
-  const {
-    data: users,
-    error,
-    isLoading,
-  } = useQuery<User[]>({
-    queryKey: ["users"],
-    queryFn: () => axios.get("/api/users").then((res) => res.data),
-    staleTime: 60 * 1000, //60s
-    retry: 3,
-  });
+  const { data: users, error, isLoading } = useUsers();
 
   if (isLoading) return <Skeleton />;
   if (error) return null;
 
+  const assignIssue = (userId: string) => {
+    axios
+      .patch("/api/issues/" + issue.id, {
+        assignedToUserId: userId === "unassigned" ? null : userId,
+      })
+      .catch(() => {
+        toast.error("Changes could not be saved.");
+      });
+    setAssigneeField(userId);
+  };
+
   return (
     <>
-      <Select.Root
-        value={assigneeField}
-        onValueChange={(userId) => {
-          axios
-            .patch("/api/issues/" + issue.id, {
-              assignedToUserId: userId === "unassigned" ? null : userId,
-            })
-            .catch(() => {
-              toast.error("Changes could not be saved.");
-            });
-          setAssigneeField(userId);
-        }}
-      >
+      <Select.Root value={assigneeField} onValueChange={assignIssue}>
         <Select.Trigger placeholder="Assign..." />
         <Select.Content>
           <Select.Group>
@@ -57,5 +47,12 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
     </>
   );
 };
+const useUsers = () =>
+  useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    staleTime: 60 * 1000, //60s
+    retry: 3,
+  });
 
 export default AssigneeSelect;
