229b84fd26a197a44b335775900d27800d0132ff
Valeria Trofim <valeria.trofim17@gmail.com>
2024-08-23 18:01:37 +0200

    Filter issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,42 +1,63 @@
-import prisma from '@/prisma/client';
-import {Table } from '@radix-ui/themes';
-import Link from '../../components/Link';
-import IssueStatusBadge from '../../components/IssueStatusBadge';
-import IssueActions from './IssueActions';
+import prisma from "@/prisma/client";
+import { Table } from "@radix-ui/themes";
+import Link from "../../components/Link";
+import IssueStatusBadge from "../../components/IssueStatusBadge";
+import IssueActions from "./IssueActions";
+import { Status } from "@prisma/client";
 
+interface Props {
+  searchParams: { status: Status };
+}
 
-  const IssuesPage = async () => {
-    const issues = await prisma.issue.findMany();
-   
+const IssuesPage = async ({ searchParams }: Props) => {
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+
+  const issues = await prisma.issue.findMany({
+    where: {
+      status,
+    },
+  });
+
   return (
     <div>
-     <IssueActions />
-  <Table.Root variant='surface'>
-    <Table.Header>
-      <Table.Row>
-        <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-        <Table.ColumnHeaderCell className='hidden md:table-cell'>Status</Table.ColumnHeaderCell>
-        <Table.ColumnHeaderCell className='hidden md:table-cell'>Created</Table.ColumnHeaderCell>
-      </Table.Row>
-    </Table.Header>
-    <Table.Body>
-      {issues.map(issue => (
-        <Table.Row key={issue.id}>
-          <Table.Cell>
-          <Link href={`/issues/${issue.id}`}>
-                  {issue.title}
-                </Link>
-            <div className='block md:hidden'><IssueStatusBadge status={issue.status} /></div>
-          </Table.Cell>
-          <Table.Cell className='hidden md:table-cell'><IssueStatusBadge status={issue.status} /></Table.Cell>
-          <Table.Cell className='hidden md:table-cell'>{issue.createdAt.toDateString()}</Table.Cell>
-        </Table.Row>
-      ))}
-    </Table.Body>
-  </Table.Root>
-</div>
-  )
-}
-export const dynamic = 'force-dynamic';
+      <IssueActions />
+      <Table.Root variant="surface">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Created
+            </Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {issues.map((issue) => (
+            <Table.Row key={issue.id}>
+              <Table.Cell>
+                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+                <div className="block md:hidden">
+                  <IssueStatusBadge status={issue.status} />
+                </div>
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                <IssueStatusBadge status={issue.status} />
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.createdAt.toDateString()}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+    </div>
+  );
+};
+export const dynamic = "force-dynamic";
 
-export default IssuesPage
+export default IssuesPage;
